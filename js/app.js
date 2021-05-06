24387baeb0f0b3f766a689e402fe415bab13277a
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-05-06 09:13:31 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -3,15 +3,12 @@ let email = document.getElementById("email");
 let emailPattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log(e);
   if (!email.value.match(emailPattern) || email.value === "") {
+    e.preventDefault();
     email.classList.add("form__email-input--invalid");
     email.nextElementSibling.classList.add("input-error--visible");
-    // return false;
   } else {
     email.classList.remove("form__email-input--invalid");
     email.nextElementSibling.classList.remove("input-error--visible");
-    // windo
   }
 });
